35f89f4a09f2e5088cf2b399a42136294cc31ad8
William Mech <Billy@Williams-MacBook-Air.local>
2015-09-14 17:38:38 -0600

    just need to do homepage

--- js/home/homeService.js
+++ js/home/homeService.js
@@ -1,5 +1,24 @@
 var app = angular.module('nbaRoutes');
 
-app.service('homeService', function($http, $q, teamService){
-
+app.service('homeService', function($http, $q){
+  this.getTeamData = function (team) {
+    var dfd = $q.defer();
+    var url = 'https://api.parse.com/1/classes/' + team;
+    $http({
+      method: 'GET',
+      url: url
+    })
+    .then(function(data){
+      var results = data.data.results;
+      var wins, losses = 0;
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].won) {wins++}
+        else {losses++};
+      };
+      results.wins = wins;
+      results.losses = losses;
+      dfd.resolve(results);
+    });
+    return dfd.promise;
+  }
 });
